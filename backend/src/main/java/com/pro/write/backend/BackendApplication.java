ba74b3c74f3a884847cf45574bf5375e7ada5412
Hardik <hardikjiio25@gmail.com>
2025-07-24 15:28:39 +0530

    disabling .env for render

--- backend/src/main/java/com/pro/write/backend/BackendApplication.java
+++ backend/src/main/java/com/pro/write/backend/BackendApplication.java
@@ -6,19 +6,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource(value = "classpath:.env", ignoreResourceNotFound = true)
+//@PropertySource(value = "classpath:.env", ignoreResourceNotFound = true)
 public class BackendApplication {
 
     public static void main(String[] args) {
 
-        Dotenv dotenv = Dotenv.load();
-
-        System.setProperty("GEMINI_API_URL", dotenv.get("GEMINI_API_URL"));
-        System.setProperty("GEMINI_API_KEY", dotenv.get("GEMINI_API_KEY"));
-
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+//        Dotenv dotenv = Dotenv.load();
+//
+//        System.setProperty("GEMINI_API_URL", dotenv.get("GEMINI_API_URL"));
+//        System.setProperty("GEMINI_API_KEY", dotenv.get("GEMINI_API_KEY"));
+//
+//        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+//        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+//        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
         SpringApplication.run(BackendApplication.class, args);
     }
